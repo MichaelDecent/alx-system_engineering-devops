ebc0f07a23a78ece69cd435287751a197f2770ef
MichaelDecent <michaeldecent2@gmail.com>
2023-08-28 02:24:52 +0000

    added a manifest to configure SSH

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,15 @@
+# configuring the SSH configuration file
+include stdlib
 
-file {'~/.ssh/school':
-content => 'Host ubuntu
-    PasswordAuthentication no
-    IdentityFile ~/.ssh/school
-'
+file_line {'Password_auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no'
 }
+
+file_line {'required_file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
+
