26e01f979aa57e49a555dade6d57236df2dec8df
MichaelDecent <michaeldecent2@gmail.com>
2023-09-05 13:31:16 +0000

    added a manifest installs Nginx and adds a custom header to Nginx

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,58 +1,49 @@
 # this manifest installs Nginx and adds a custom header to Nginx
-web_server {
-  exec {'apt-update':
-  command => 'sudo apt update',
-  }
 
-  #installs nginx
-  package { 'nginx':
-    ensure          => 'installed',
-    provider        => 'apt',
-    install_options => ['--force-yes'],
-    }
-
-  # adds fire wall
-  firewall { 'allow nginx':
-    port   => 80,
-    proto  => 'tcp',
-    action => 'accept',
-  }
-
-  # Configres Nginx with a customized header
-  file { '/etc/nginx/sites-available/default':
-    ensure  => 'present',
-    content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    
-    add_header X-Served-By \$hostname;
-
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-
-    server_name _;
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
+exec {'apt-update':
+  command => '/usr/bin/apt update',
 }
-",
-    notify  => Exec['nginx-test'],
+
+#installs nginx
+package {'nginx':
+  ensure          => 'installed',
+  provider        => 'apt',
+  install_options => ['--force-yes'],
   }
 
-  # Test the configuration file of Nginx
-  exec { 'nginx-test':
-    command     => 'sudo nginx -t',
-    refreshonly => true,
-    notify      => Exec['nginx-restart'],
+# Configres Nginx with a customized header
+file {'/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => '
+server {
+  listen 80 default_server;
+  listen [::]:80 default_server;
+
+  add_header X-Served-By \$hostname;
+
+  root /var/www/html;
+  index index.html index.htm index.nginx-debian.html;
+
+  server_name _;
+
+  location / {
+      try_files \$uri \$uri/ =404;
   }
+}
+',
+  notify  => Exec['nginx-test'],
+}
+
+# Test the configuration file of Nginx
+exec {'nginx-test':
+  command     => '/usr/sbin/nginx -t',
+  refreshonly => true,
+  require     => Package['nginx'],
+  notify      => Exec['nginx-restart'],
+}
 
   # Restarts Nginx 
-  exec { 'nginx-restart':
-    command     => 'sudo service nginx restart',
-    refreshonly => true,
+exec {'nginx-restart':
+  command     => '/usr/sbin/service nginx restart',
+  refreshonly => true,
   }
-
-}
-
