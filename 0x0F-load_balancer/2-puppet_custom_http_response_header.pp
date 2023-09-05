27c65ff79469f681ad0a75a18ed0d27b3bd491a3
MichaelDecent <michaeldecent2@gmail.com>
2023-09-05 14:06:41 +0000

    added a manifest installs Nginx and adds a custom header to Nginx

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,49 +1,26 @@
-# this manifest installs Nginx and adds a custom header to Nginx
-
-exec {'apt-update':
-  command => '/usr/bin/apt update',
+# this manifest installs Nginx and adds a custom header to Nginx 
+ 
+exec {'apt-update': 
+  command => '/usr/bin/apt update', 
+} 
+ 
+# installs nginx 
+package {'nginx': 
+  ensure          => 'installed', 
+  provider        => 'apt', 
+  install_options => ['--force-yes'], 
+} 
+ 
+# Configres Nginx with a customized header 
+file_line {'add header': 
+  ensure => 'present', 
+  path   => '/etc/nginx/sites-available/default', 
+  line   => '\tadd_header X-Served-By ${hostname};', 
+  after  => 'server_name _;', 
+} 
+ 
+# starts running nginx 
+service {'nginx': 
+  ensure => 'running', 
+  require => Package['nginx'], 
 }
-
-#installs nginx
-package {'nginx':
-  ensure          => 'installed',
-  provider        => 'apt',
-  install_options => ['--force-yes'],
-  }
-
-# Configres Nginx with a customized header
-file {'/etc/nginx/sites-available/default':
-  ensure  => 'present',
-  content => '
-server {
-  listen 80 default_server;
-  listen [::]:80 default_server;
-
-  add_header X-Served-By \$hostname;
-
-  root /var/www/html;
-  index index.html index.htm index.nginx-debian.html;
-
-  server_name _;
-
-  location / {
-      try_files \$uri \$uri/ =404;
-  }
-}
-',
-  notify  => Exec['nginx-test'],
-}
-
-# Test the configuration file of Nginx
-exec {'nginx-test':
-  command     => '/usr/sbin/nginx -t',
-  refreshonly => true,
-  require     => Package['nginx'],
-  notify      => Exec['nginx-restart'],
-}
-
-  # Restarts Nginx 
-exec {'nginx-restart':
-  command     => '/usr/sbin/service nginx restart',
-  refreshonly => true,
-  }
