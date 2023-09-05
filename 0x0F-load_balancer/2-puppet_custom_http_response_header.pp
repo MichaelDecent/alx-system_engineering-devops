7c0a1c527329a5eb08ce379c03d8fcbdb64b1ea9
MichaelDecent <michaeldecent2@gmail.com>
2023-09-05 14:11:55 +0000

    added a manifest installs Nginx and adds a custom header to Nginx

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,26 +1,17 @@
-# this manifest installs Nginx and adds a custom header to Nginx 
- 
-exec {'apt-update': 
-  command => '/usr/bin/apt update', 
-} 
- 
-# installs nginx 
-package {'nginx': 
-  ensure          => 'installed', 
-  provider        => 'apt', 
-  install_options => ['--force-yes'], 
-} 
- 
-# Configres Nginx with a customized header 
-file_line {'add header': 
-  ensure => 'present', 
-  path   => '/etc/nginx/sites-available/default', 
-  line   => '\tadd_header X-Served-By ${hostname};', 
-  after  => 'server_name _;', 
-} 
- 
-# starts running nginx 
-service {'nginx': 
-  ensure => 'running', 
-  require => Package['nginx'], 
+# Installs Nginx and custom HTTP response headers
+exec { '/usr/bin/env apt-get -y update' : }
+-> package { 'nginx':
+  ensure => installed,
+}
+-> file { '/var/www/html/index.html' :
+  content => 'Holberton School!',
+}
+-> file_line { 'add header' :
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name _;',
+}
+-> service { 'nginx':
+  ensure => running,
 }
