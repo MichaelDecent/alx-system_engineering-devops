7598380a8bb6e5b249af3684ae46ad52a37dba34
MichaelDecent <michaeldecent2@gmail.com>
2023-09-05 14:14:14 +0000

    added a manifest installs Nginx and adds a custom header to Nginx

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,9 +3,6 @@ exec { '/usr/bin/env apt-get -y update' : }
 -> package { 'nginx':
   ensure => installed,
 }
--> file { '/var/www/html/index.html' :
-  content => 'Holberton School!',
-}
 -> file_line { 'add header' :
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
