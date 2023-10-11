5da93bfff8cec9d75257f195d376bb89028ac47b
MichaelDecent <michaeldecent2@gmail.com>
2023-10-11 14:26:15 +0000

    added a python script to query Reddit api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module contains a  function that queries the
+Reddit API and returns the number of subscribers
+"""
+import requests
+import json
+
+
+def number_of_subscribers(subreddit):
+    """
+    a function that queries the Reddit API
+    and returns the number of subscribers
+    """
+    url = f"https://www.reddit.com//r/{subreddit}/about.json"
+
+    try:
+        response = requests.get(url)
+        subreddit_info = response.json()
+        return subreddit_info['data']['subscribers']
+    except Exception as e:
+        return 0
+
+if __name__ == "__main__":
+    subreddit_name = "python"
+    print(number_of_subscribers(subreddit_name))
